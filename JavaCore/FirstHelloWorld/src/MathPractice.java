cea6a7466fc4054d3ac272fa15ff44089994318d
sherzodAlxibekov <you@example.com>
2022-06-04 16:41:41 +0500

    Java add

---
+++ JavaCore/FirstHelloWorld/src/MathPractice.java
@@ -0,0 +1,15 @@
+public class MathPractice {
+    public static void main(String[] args) {
+
+        String name = "Sherzod";
+        String className = "Java Begin";
+
+        int age =32;
+        int position =5;
+
+        System.out.println("Name: " + name + "Class Name: " + className);
+        System.out.println("-------------------");
+        
+
+    }
+}
